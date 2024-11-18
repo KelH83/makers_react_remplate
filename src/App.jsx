df72e8a405bfffc5b9a176ed94cd0ee0b55c4c4a
Kelly <kellyhowes83@gmail.com>
2024-11-18 14:53:43 +0000

    Added event listeners

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Profile from "./components/Profile";
 import Product from "./components/Product"
+import ClickListener from "./components/ClickListener"
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Profile name="Kelly" job="Junior software engineer apprentice" birthday="31st August"/>
       <Product name="Air Fryer K2000" description="The best air fryer to fry all things, even Mars bars" price={150}/>
       <Product name="Microwave" description="It cooks things quickly" price={120}/>
+      <ClickListener />
     </>
   );
 }
